7c1c4429da18ea6c5354e951c766e10442bd34eb
gugu <psdevco@gmail.com>
2025-10-19 15:07:34 -0400

    initial install

--- react/app/src/main.jsx
+++ react/app/src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
